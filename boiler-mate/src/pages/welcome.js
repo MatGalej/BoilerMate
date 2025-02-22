3cc57c2d17a1303d02aff502ad884a8fa3a05600
MatGalej <mgalej@purdue.edu>
2025-02-21 23:26:50 -0500

    doce

---
+++ boiler-mate/src/pages/welcome.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function welcome() {
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default welcome;
