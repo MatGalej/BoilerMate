91eb386eccbbea33e3ff02f05f0276dc1c66d39d
Nick Wu <wu2264@purdue.edu>
2025-02-21 23:00:45 -0500

    fixed db

--- boiler-mate/src/App.js
+++ boiler-mate/src/App.js
@@ -1,14 +1,14 @@
-// src/App.js
 import React from "react";
-import { db, auth } from "./firebaseConfig";
+import { firestore, auth } from "./firebaseConfig"; // Use 'firestore' instead of 'db'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  console.log("🔥 Firebase DB:", db);
+  console.log("🔥 Firestore (DB):", firestore);
   console.log("🔥 Firebase Auth:", auth);
+
   return (
     <Router>
       <Routes>
